d6886fbe59a45609e8b16ba487c96a7e17b63a54
xXZyzzXx <officialtopdoge@gmail.com>
2021-01-27 01:11:08 +0200

    add testingConfig

--- application/config.py
+++ application/config.py
@@ -3,11 +3,16 @@ import os
 
 class Config(object):
     DEBUG = False
+    TESTING = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default secret')
 
 
 class ProductionConfig(Config):
     DEBUG = False
+
+
+class TestingConfig(Config):
+    TESTING = True
 
 
 class DevelopmentConfig(Config):
@@ -17,5 +22,6 @@ class DevelopmentConfig(Config):
 configs = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
+    'testing': TestingConfig,
     'default': DevelopmentConfig
 }
