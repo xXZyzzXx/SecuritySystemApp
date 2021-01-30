906837c83e3be1ba5e45675f41c674eccb53437e
xXZyzzXx <officialtopdoge@gmail.com>
2021-01-30 14:08:44 +0200

    testing encryption

--- application/tests.py
+++ application/tests.py
@@ -9,22 +9,19 @@ from encrypt import encrypt_message, decrypt_message
 class TestAlarm(unittest.TestCase):
 
     def setUp(self):
-        self.app = app  # Need to change force_https to False
+        self.app = app
         self.app.config['TESTING'] = True
         self.client = app.test_client()
-        self.data = {'status': 'reload'}
-        self.app_id = 1234
+        self.data = {'alarm': 'caution'}
+        self.app_id = random.randint(1000, 9999)
 
     def test_successful_encrypt(self):
-        # Given
         encrypted = encrypt_message(self.data)
-
-        response = self.client.post(f'/api/alarm/{self.app_id}', data=encrypted)  # Return None, 302 Error
-
-        r = response.json()
+        response = self.client.post(f'/api/alarm/{self.app_id}', data={"key": encrypted})
+        r = response.get_json()
         decrypt = decrypt_message(r['key'].encode())
         answer = json.loads(decrypt)
-        # Then
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(answer['command'], 'reload')
 
